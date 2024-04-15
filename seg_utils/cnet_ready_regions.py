a26ddd0f6f9e5cec25cdfaf2f16a8ed5479afc58
laurensharwood <49936897+laurensharwood@users.noreply.github.com>
2024-04-15 09:41:11 -0700

    fix reading VI list / array

--- seg_utils/cnet_ready_regions.py
+++ seg_utils/cnet_ready_regions.py
@@ -5,10 +5,8 @@ import cnet_funcs as cf
 
 
 def main():
-    VI_list = sys.argv[2]
-    vis = VI_list[1:-1].replace(" ", "").split(",")
     ## take training regions that have folders in time_series_vars (are ready) and make a list to populate config_cultionet.yml
-    cf.ready_regions(version_dir=sys.argv[1], vi_list=vis)
+    cf.ready_regions(version_dir=sys.argv[1], vi_list=sys.argv[2].split(","))
 
 if __name__ == "__main__":
     main()
